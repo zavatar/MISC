e016f609c1291c66d76cd1416e6d70335ad4c227
zhumeng1989 <zhumeng1989@gmail.com>
2013-06-30 11:27:07 +0800

    fix use delete atomic function

--- samples/multi_threads.cpp
+++ samples/multi_threads.cpp
@@ -5,16 +5,17 @@
 //////////////////////////////////////////////////////////////////////////
 #include <misc/misc.hpp>
 #include <vector>
-#include <atomic>
 #include <thread>
 
-void call_from_thread(std::atomic<int> &sum, int i) {
+void call_from_thread(int &sum, int i) {
+	printf("%d, ", sum);
 	sum += i;
+	printf("%d\n", sum);
 }
 
 int main() {
 	std::vector<std::thread> threads;
-	std::atomic<int> asum(0);
+	int asum(0);
 	for (int i=0; i<8; i++) {
 		threads.push_back(std::thread(call_from_thread, std::ref(asum), i));
 	}
