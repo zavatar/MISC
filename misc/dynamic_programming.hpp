8eb43f2d74a97fa6af12b7ef1916e81ed9ace738
zhumeng <zhumeng1989@gmail.com>
2013-08-31 16:33:15 +0800

    Add Dynamic Programming Module

---
+++ misc/dynamic_programming.hpp
@@ -0,0 +1,13 @@
+#ifndef misc_dynamic_programming
+#define misc_dynamic_programming
+
+namespace misc {
+
+
+} // misc
+
+#ifndef MISC_EXTERNAL_TEMPLATE
+#include "dynamic_programming.inl"
+#endif // !MISC_EXTERNAL_TEMPLATE
+
+#endif // !misc_dynamic_programming
