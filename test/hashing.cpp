60688e8f12b1ff41ed8d39c21e65cc0056b4ecb3
zhumeng <zhumeng1989@gmail.com>
2013-08-31 15:29:26 +0800

    Add FNV_1a hashing

--- test/hashing.cpp
+++ test/hashing.cpp
@@ -2,3 +2,19 @@
 
 #include "gtest/gtest.h"
 
+TEST(FNV_1a_Test, Standard)
+{
+	srand( (unsigned int)time(NULL) );
+	int val = rand();
+	EXPECT_EQ(misc::FNV_1a<int>()(val), misc::FNV_1a<int>()(val));
+
+	bool tmp[4] = {1,1,1,0};
+	EXPECT_EQ(misc::FNV_1a<bool>()(tmp[0]), misc::FNV_1a<bool>()(tmp[2]));
+	EXPECT_EQ(misc::FNV_1a<bool>()(false), misc::FNV_1a<bool>()(false));
+
+	// bool*
+	EXPECT_NE(misc::FNV_1a<bool*>()(&tmp[0]), misc::FNV_1a<bool*>()(&tmp[1]));
+
+	// float
+	EXPECT_EQ(misc::FNV_1a<bool>()(.0f), misc::FNV_1a<bool>()(-.0f));
+}
