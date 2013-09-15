1858dcf55ea53102adc8361bb2c94aa83374ae90
zhumeng1989 <zhumeng1989@gmail.com>
2013-09-15 17:19:49 +0800

    loki samples

--- samples/loki_modern_cplusplus.cpp
+++ samples/loki_modern_cplusplus.cpp
@@ -5,8 +5,98 @@
 
 #include <misc/misc.hpp>
 
+//#include <loki/static_check.h>
 #include <loki/Typelist.h>
+//#include <loki/TypelistMacros.h>
 #include <loki/HierarchyGenerators.h>
+//#include <loki/Functor.h>
+//#include <loki/EmptyType.h>
+//#include <loki/TypeTraits.h>
+
+// Chapter 1. Policy-Based Class Design
+// AOP
+
+// Chapter 2. Techniques
+
+// 2.1 Compile-Time Assertions
+BOOST_STATIC_ASSERT(true);
+#ifdef MCXX11
+// C++11
+static_assert(true, "Msg");
+#endif
+// Loki Error?
+//LOKI_STATIC_CHECK(true, "Msg");
+
+// 2.3 Local Classes
+class Interface 
+{ 
+public: 
+	virtual void Fun() = 0; 
+};
+// Adapter
+template <class T, class P> 
+Interface* MakeAdapter(const T& obj, const P& arg) 
+{ 
+	class Local : public Interface 
+	{ 
+	public: 
+		Local(const T& obj, const P& arg) 
+			: obj_(obj), arg_(arg) {} 
+		virtual void Fun() 
+		{ 
+			obj_.Call(arg_); 
+		} 
+	private: 
+		T obj_; 
+		P arg_; 
+	}; 
+	return new Local(obj, arg); 
+}
+
+// 2.6 Type Selection
+template <bool flag, typename T, typename U> 
+struct Select 
+{
+	typedef T Result; 
+}; 
+template <typename T, typename U> 
+struct Select<false, T, U> 
+{ 
+	typedef U Result; 
+};
+template <typename T, bool isPolymorphic> 
+class NiftyContainer 
+{ 
+	typedef typename Select<isPolymorphic, T*, T>::Result 
+		ValueType; 
+}; 
+
+// 2.7 Detecting Convertibility and Inheritance at Compile Time
+template <class T, class U> 
+class Conversion 
+{ 
+	typedef char Small; 
+	class Big { char dummy[2]; }; 
+	static Small Test(U);
+	static Big Test(...); 
+	static T MakeT(); 
+public: 
+	enum { exists = 
+		sizeof(Test(MakeT())) == sizeof(Small) }; 
+	enum { sameType = false };
+};
+template <class T> 
+class Conversion<T, T> 
+{ 
+public: 
+	enum { exists = 1, sameType = 1 }; 
+}; 
+#define SUPERSUBCLASS(T, U) \
+	(::mc::Conversion<const U*, const T*>::exists && \
+	!::mc::Conversion<const T*, const void*>::sameType)
+#define SUPERSUBCLASS_STRICT(T, U) \
+	(SUPERSUBCLASS(T, U) && \
+	!::mc::Conversion<const T, const U>::sameType)
 
 namespace TlPrivate {
 #if MISC_ISGCC
@@ -53,7 +143,53 @@ namespace TlPrivate {
 	};
 
 }
+namespace FunPrivate {
 
+	void TestFunction(int);
+	void TestFunction(int i, double d)
+	{
+		std::cout << "TestFunction(" << i 
+			<< ", " << d << ") called.\n";
+	}
+
+	struct TestFunctor
+	{
+		void operator()(int i, double d) 
+		{ 
+			std::cout << "TestFunctor::operator()(" << i 
+				<< ", " << d << ") called.\n"; 
+		}
+	};
+
+	template <typename T>
+	class Functor
+	{
+	public:
+		// const is important
+		Functor(const T& fun) : _f(fun) {}
+
+		void operator()(int i, double d)
+		{
+			_f(i, d);
+		}
+
+	private:
+		T _f;
+	};
+
+	class Parrot 
+	{ 
+	public: 
+		void Eat() 
+		{ 
+			std::cout << "Tsk, knick, tsk...\n"; 
+		} 
+		void Speak() 
+		{ 
+			std::cout << "Oh Captain, my Captain!\n"; 
+		} 
+	};
+}
 int main()
 {
 #if MISC_ISGCC
@@ -135,5 +271,55 @@ int main()
 	// 1*4
 	std::cout << sizeof(MyEventHandler) << std::endl;
 
+//////////////////////////////////////////////////////////////////////////
+/*
+	// Loki::Functor
+	// Function
+	//Loki::Functor<void, LOKI_TYPELIST_2(int, double)> cmd1(FunPrivate::TestFunction);
+	//cmd1(4, 4.5);
+	// operate()
+	FunPrivate::TestFunctor f;
+	Loki::Functor<void, LOKI_TYPELIST_2(int, double)> cmd2(f);
+	cmd2(4, 4.5);
+	// Functor
+	Loki::Functor<void, LOKI_TYPELIST_2(int, double)> cmd3(cmd2);
+	cmd3(4, 4.5);
+	// FunPrivate::Functor
+	// 	// Function
+	// 	FunPrivate::Functor<void(*)(int, double)> fun1(FunPrivate::TestFunction);
+	// 	fun1(4, 4.5);
+	// operate()
+	FunPrivate::Functor<FunPrivate::TestFunctor> fun2(f);
+	fun2(4, 4.5);
+	// Functor
+	FunPrivate::Functor<FunPrivate::Functor<FunPrivate::TestFunctor>> fun3(fun2);
+	fun3(4, 4.5);
+
+	typedef void (*TpFun)(int, double);
+	// C++11 Alias templates
+	//using TpFun = void(*)(int, double);
+	// Method 1:
+	TpFun pF = FunPrivate::TestFunction;
+	Loki::Functor<void, LOKI_TYPELIST_2(int, double)> cmd4(pF);
+	cmd4(4, 4.5);
+	// Method 2:
+	Loki::Functor<void, LOKI_TYPELIST_2(int, double)> cmd5(
+		static_cast<TpFun>(FunPrivate::TestFunction));
+	cmd5(4, 4.5);
+
+	typedef void (FunPrivate::Parrot::* TpMemFun)();
+	TpMemFun pActivity = &FunPrivate::Parrot::Eat;
+	FunPrivate::Parrot geronimo; 
+	FunPrivate::Parrot* pGeronimo = &geronimo;
+	(geronimo.*pActivity)(); 
+	(pGeronimo->*pActivity)();
+	pActivity = &FunPrivate::Parrot::Speak;
+	(geronimo.*pActivity)();
+
+	Loki::Functor<> cmd6(&geronimo, &FunPrivate::Parrot::Eat),
+		cmd7(&geronimo, &FunPrivate::Parrot::Speak);
+	cmd6();
+	cmd7();
+	*/
 	return 0;
 }
